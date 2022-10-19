7674101d163aae34544ce51421de6993c6ccf5cb
parkjh <incon@incon.kr>
2022-10-19 17:20:10 +0900

    second

---
+++ config/dev.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoURI: 'mongodb+srv://junheefr789:rka3695159!@junheefr789.pulw7eo.mongodb.net/?retryWrites=true&w=majority'
+}
--- index.js
+++ index.js
@@ -1,14 +1,31 @@
-const express = require('express')
-const app = express()
-const port = 5000
+const express = require('express');
+const app = express();
+const port = 5000;
+const bodyParser = require('body-parser');
+const {User} = require('./models/User');
+
+const config = require('./config/key')
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.use(bodyParser.json());
 
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://junheefr789:rka3695159!@junheefr789.pulw7eo.mongodb.net/?retryWrites=true&w=majority',{
+mongoose.connect(config.mongoURI,{
     useNewUrlParser: true,
     useUnifiedTopology:true
 }).then(()=>console.log('MongoDb Connected...'))
 .catch(err => console.log(err))
 
-app.get('/', (req, res) => res.send('Hello World'))
+app.get('/', (req, res) => res.send('Hello World! welcome~~'))
+
+app.post('/register',(req,res) =>{
+    const user = new User(req.body) 
+    user.save((err, userInfo)=>{
+        if(err) return res.json({success:false, err})
+        return res.status(200).json({success:true})
+    })
+
+})
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
